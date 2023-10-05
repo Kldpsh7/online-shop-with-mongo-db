532dc8fffc3e965f760ba83fe850ab69bb9759b5
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-10-06 00:07:25 +0530

    add to cart available

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title,price,imageUrl,description);
+  const userId = req.user._id
+  console.log(req.user._id)
+  const product = new Product(title,price,imageUrl,description,userId);
   product.save()
     .then(result => {
       // console.log(result);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,48 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User{
+    constructor(name,email,phone,cart,id){
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        this.cart=cart;
+        this._id=id;
+    }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+    save(){
+        const db = getDb();
+        return db.collection('users').insertOne(this)
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err))
+    }
+
+    addToCart(product){
+        const cartProduct = this.cart.items.findIndex(cp=>{
+            return cp.productId.toString()===product._id.toString()
+        });
+        const db = getDb();
+        let updatedCart = this.cart
+        if(cartProduct>-1){
+            updatedCart.items[cartProduct].quantity+=1;
+        }
+        else{
+            updatedCart = {items:[...updatedCart.items,{productId: new mongodb.ObjectId(product._id),quantity:1}]};
+        }
+        return db.collection('users').updateOne(
+            {_id:new mongodb.ObjectId(this._id)},
+            { $set : {cart:updatedCart}}
+            );
+    }
+
+    static findById(userId){
+        const db = getDb();
+        return db.collection('users').find({_id:new mongodb.ObjectId(userId)}).next()
+        .then()
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+}
 
 module.exports = User;
