c4bbe146c7faa1aa2e9617f8596b77bf23ec7224
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-10-06 22:26:25 +0530

    orders page working

--- models/user.js
+++ models/user.js
@@ -68,7 +68,16 @@ class User{
 
     addOrder(){
         const db = getDb();
-        return db.collection('orders').insertOne(this.cart)
+        return this.getCart().then(products=>{
+            const order = {
+                items:products,
+                user:{
+                    _id:new mongodb.ObjectId(this._id),
+                    name:this.name
+                }
+            }
+            return db.collection('orders').insertOne(order)
+        })
         .then(result=>{
             this.cart= {items:[]};
             return db.collection('users').updateOne(
@@ -77,6 +86,11 @@ class User{
                 );
         })
         .catch(err=>console.log(err));
+    }
+
+    getOrders(){
+        const db = getDb();
+        return db.collection('orders').find({'user._id':new mongodb.ObjectId(this._id)}).toArray();
     }
 
     static findById(userId){
