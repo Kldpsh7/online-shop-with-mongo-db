dc7bc46f22ac2c1c4aede25378916882189ef613
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-10-07 12:28:36 +0530

    User schema added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("651ed8ffb75d16622b07fd6b")
-//     .then(user => {
-//       req.user = new User(user.name,user.email,user.phone,user.cart,user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("6520fda219d15b001467a44a")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,5 +35,15 @@ app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://kldpsh7:${process.env.MONGOPASS}@shop.iqmcj0i.mongodb.net/shop?retryWrites=true&w=majority`)
 .then(()=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user = new User({
+        name:'Kuldeep Sahrma',
+        email:'kldpsh77@gmail.com',
+        crat:[]
+      })
+      user.save();
+    }
+  })
   app.listen(3000,()=>console.log('listening on 3000'));
 }).catch(err=>console.log(err));
